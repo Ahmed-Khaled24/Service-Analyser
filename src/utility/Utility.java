86aeb8c1c08912f40e5ff7fa714d4101b13d8557
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-05-05 23:56:36 +0200

    Create two functions: 1. constructAPI returns reference to API object after construct it using the given excel data rows. 2. constructFieldObject that construct Field object and store it in the right place in the given API.

--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -1,4 +1,5 @@
 package utility;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,6 +17,29 @@ public class Utility {
         catch(FileNotFoundException e){
             throw new IOException("File not found.") ;
         }
+    }
+
+    public static void constructFieldObject(XSSFRow thisRow, API currentAPI){
+
+        // ---- Indices of each property ----
+        final int IO_index = 0;
+        final int fieldName_index = 1;
+        final int type_index = 2;
+        final int allowedValues_index = 3;
+        final int mandatory_index = 4;
+        // -------------------------------------------------
+
+        // To do: complete the function
+    }
+
+    public static void constructAPI(XSSFRow nameRow, XSSFRow theOtherRow){ // theOtherRow = nameRow + 2
+        // ---- Indices of each property ----
+        final int name_index = 0; // in nameRow
+        final int HTTP_index = 0; // in theOtherRow
+        final int URL_index  = 1; // in theOtherRow
+        // -------------------------------------------------
+
+        // To do: complete the function
     }
 
 }
