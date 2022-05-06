bf12b6b22e18136b793de2ca17a35971e5ca6630
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-05-06 16:33:44 +0200

    Finish constructAPI function in Utility class

--- src/utility/Utility.java
+++ src/utility/Utility.java
@@ -1,4 +1,6 @@
 package utility;
+import components.API;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
@@ -20,7 +22,6 @@ public class Utility {
     }
 
     public static void constructFieldObject(XSSFRow thisRow, API currentAPI){
-
         // ---- Indices of each property ----
         final int IO_index = 0;
         final int fieldName_index = 1;
@@ -32,14 +33,26 @@ public class Utility {
         // To do: complete the function
     }
 
-    public static void constructAPI(XSSFRow nameRow, XSSFRow theOtherRow){ // theOtherRow = nameRow + 2
-        // ---- Indices of each property ----
+    // Construct an API object using the given two rows and return reference to the object.
+    public static API constructAPI(XSSFRow nameRow, XSSFRow propertiesRow){ // propertiesRow = nameRow + 2
+        // ---- Index of each property ----
         final int name_index = 0; // in nameRow
-        final int HTTP_index = 0; // in theOtherRow
-        final int URL_index  = 1; // in theOtherRow
+        final int HTTP_index = 0; // in propertiesRow
+        final int URL_index  = 1; // in propertiesRow
         // -------------------------------------------------
 
-        // To do: complete the function
+        // Hold the wanted cells in temporary variables.
+        XSSFCell nameCell = nameRow.getCell(name_index);
+        XSSFCell HTTPCell = propertiesRow.getCell(HTTP_index);
+        XSSFCell URLCell = propertiesRow.getCell(URL_index);
+
+        // Extract the data from the cells into strings.
+        String name = nameCell.toString();
+        String operation = HTTPCell.toString();
+        String REST_URL = URLCell.toString();
+
+        // Construct and return the API.
+        return new API(name, operation, REST_URL);
     }
 
 }
