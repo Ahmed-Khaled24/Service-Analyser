38e3e2eff19239c3d16db21f5d49e8f332277ffc
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-05-06 18:25:27 +0200

    Add constructor to StringField class.

--- src/components/StringField.java
+++ src/components/StringField.java
@@ -1,4 +1,10 @@
 package components;
-public final class StringField extends Field{
 
+
+import java.util.ArrayList;
+
+public final class StringField extends Field{
+    public StringField(String type, boolean mandatory, ArrayList<String> allowedValues, String name, String parentFieldName, char IO){
+        super(type, mandatory, allowedValues, name, parentFieldName, IO);
+    }
 }
